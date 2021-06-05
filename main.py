212702a0ef99c0e79a3c3a8e7e6e9c890011c91f
Sergi Teruel Prieto <serg.95.stp@gmail.com>
2021-06-05 16:59:47 +0200

    Fixing pylint warnings

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main module"""
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -5,6 +6,7 @@ from starlette.routing import Route
 
 
 async def homepage(request: Request):
+    """Homepage"""
     return JSONResponse({'hello': request})
 
 
