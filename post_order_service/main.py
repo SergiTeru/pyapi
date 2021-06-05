dd92731936d92a6f1e8d4aa27a233c1baf9eaebc
Sergi Teruel Prieto <serg.95.stp@gmail.com>
2021-06-05 22:59:42 +0200

    Adding code to produce amqp messages

--- post_order_service/main.py
+++ post_order_service/main.py
@@ -1,15 +1,36 @@
 """post_order_service main module"""
-from starlette.applications import Starlette
-from starlette.requests import Request
-from starlette.responses import JSONResponse
-from starlette.routing import Route
+import asyncio
+from typing import Optional
+
+import aio_pika
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-async def homepage(request: Request):
-    """Homepage"""
-    return JSONResponse({'hello': request})
+class Order(BaseModel):
+    product_code: str
+    qty: Optional[int] = 1
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage),
-])
+app = FastAPI()
+
+@app.post("/orders/")
+async def create_order(order: Order):
+    loop = asyncio.get_event_loop()
+    connection = await aio_pika.connect_robust(
+        "amqp://guest:guest@127.0.0.1/", loop=loop
+    )
+
+    routing_key = "test_queue"
+
+    channel = await connection.channel()    # type: aio_pika.Channel
+
+    await channel.default_exchange.publish(
+        aio_pika.Message(
+            body='Hello {}'.format(routing_key).encode()
+        ),
+        routing_key=routing_key
+    )
+
+    await connection.close()
+    return order
