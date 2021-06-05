dba4b4c01f5edbbceae852c0ecc023dda20d8d06
Sergi Teruel Prieto <serg.95.stp@gmail.com>
2021-06-05 16:57:25 +0200

    Fixing github actions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from starlette.applications import Starlette
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
 
-async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+async def homepage(request: Request):
+    return JSONResponse({'hello': request})
 
 
 app = Starlette(debug=True, routes=[
